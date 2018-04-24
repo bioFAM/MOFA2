f6f541e623b281839cc1383c7c50ed08804a3307
Britta Velten <britta.velten@embl.de>
2018-04-24 16:42:29 +0200

    added model comparison and selection functions

--- BioFAMtools/R/compareModels.R
+++ BioFAMtools/R/compareModels.R
@@ -5,7 +5,7 @@
 
 
 #' @title Plot the robustness of the latent factors across diferent trials
-#' @name compareModels
+#' @name compareFactors
 #' @description Different objects of \code{\link{BioFAModel}} are compared in terms of correlation between 
 #' their latent factors. The correlation is calculated only on those samples which are present in all models.
 #' Ideally, the output should look like a block diagonal matrix, suggesting that all detected factors are robust under different initialisations.
@@ -23,7 +23,7 @@
 #' @importFrom grDevices colorRampPalette
 #' @export
 
-compareModels <- function(models, comparison = "all", ...) {
+compareFactors <- function(models, comparison = "all", ...) {
   
   # Sanity checks
   if(!is.list(models))
@@ -101,4 +101,53 @@ compareModels <- function(models, comparison = "all", ...) {
     names(PairWiseCor) <- names(models[-length(models)])
     return(PairWiseCor)
   }
+}
+
+
+#' @title Compare different trained \code{\link{BioFAModel}} objects in terms of the final value of the ELBO statistics and number of inferred factors
+#' @name compareModels
+#' @description Different objects of \code{\link{BioFAModel}} are compared in terms of the final value of the ELBO statistics. 
+#' For model selection the model with the highest ELBO value is selected.
+#' @param models a list containing \code{\link{BioFAModel}} objects.
+#' @export
+
+compareModels <- function(models, show_modelnames = FALSE) {
+  # Sanity checks
+  if(!is.list(models))
+    stop("'models' has to be a list")
+  if (!all(sapply(models, function (l) class(l)=="BioFAModel")))
+    stop("Each element of the the list 'models' has to be an instance of BioFAModel")
+
+  elbo_vals <- sapply(models, getELBO)
+  n_factors <- sapply(models, function(m) {
+    n_fac <- getDimensions(m)$K
+    if(m@ModelOpts$learnIntercept) n_fac <- n_fac - 1
+    n_fac
+    })
+  if(is.null(names(models))) names(models) <- paste0("model_", seq_along(models))
+  df <- data.frame(ELBO=elbo_vals, model = names(models))
+  gg <- ggplot(df, aes(x=model,y=n_factors, fill=ELBO)) + geom_bar(stat="identity")+
+    ylab("Number of inferred factors")
+  if(show_modelnames) gg <- gg + theme(axis.text.x=element_text(angle=60, vjust=1, hjust=1))
+  else gg <- gg + theme(axis.text.x=element_blank())
+  return(gg)
+}
+
+#' @title Select a model from a list of trained \code{\link{BioFAModel}} objects based on the best ELBO value
+#' @name select_model
+#' @description Different objects of \code{\link{BioFAModel}} are compared in terms of the final value of the ELBO statistics 
+#' and the model with the highest ELBO value is selected.
+#' @param models a list containing \code{\link{BioFAModel}} objects.
+#' @export
+
+selectModel <- function(models, plotit =TRUE) {
+  # Sanity checks
+  if(!is.list(models))
+    stop("'models' has to be a list")
+  if (!all(sapply(models, function (l) class(l)=="BioFAModel")))
+    stop("Each element of the the list 'models' has to be an instance of BioFAModel")
+
+  elbo_vals <- sapply(models, getELBO)
+  if(plotit) compareModels(models)
+  models[[which.max(elbo_vals)]]
 }
