71498665781c39cba23be023797659beb1f7a21c
damien.arnol <damien.arnol@gmail.com>
2018-04-27 17:38:33 +0100

    minor

--- biofam/core/nodes/Y_nodes.py
+++ biofam/core/nodes/Y_nodes.py
@@ -80,7 +80,7 @@ class Y_Node(Constant_Variational_Node):
         if self.opts['tau_d']:
             lik = self.likconst + 0.5 * s.sum(Tau["lnE"]) - s.dot(Tau["E"][0,:], tmp)
         else:
-            print 'non implemented'
+            print('non implemented')
             return 0
             lik = self.likconst + 0.5 * s.sum(self.D * (Tau["lnE"])) - s.dot(Tau["E"], tmp)
 
