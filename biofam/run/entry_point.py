fc9893b4d10c6fe26bb32616e87a526acb6b2324
damien.arnol <damien.arnol@gmail.com>
2018-06-06 12:10:18 +0100

    minor

--- biofam/run/entry_point.py
+++ biofam/run/entry_point.py
@@ -525,6 +525,10 @@ class entry_sfa(entry_point):
 
 if __name__ == '__main__':
     ent = entry_point()
+    # dir = '/Users/damienarnol1/Documents/local/pro/PhD/FA/biofam/paper_figures/simul_data/stochastic_simul/'
+    # infiles = [dir+'/data_0_0.txt', dir+'/data_1_0.txt', dir+'/data_2_0.txt']
+    # views =  ["view_0", "view_1", "view_2"]
+    # groups = ["group_0", "group_0", "group_0"]
 
     infiles = ["../run/test_data/with_nas/500_0.txt", "../run/test_data/with_nas/500_1.txt", "../run/test_data/with_nas/500_2.txt", "../run/test_data/with_nas/500_2.txt" ]
     views =  ["view_A", "view_A", "view_B", "view_B"]
@@ -540,7 +544,7 @@ if __name__ == '__main__':
     #
     ent.set_data_options(lik, center_features=True, center_features_per_group=False, scale_features=False, scale_views=True)
     ent.set_data_from_files(infiles, views, groups, delimiter=" ", header_cols=False, header_rows=False)
-    ent.set_model_options(ard_z=False, sl_w=False, sl_z=False, ard_w=False, factors=15, likelihoods=lik, learnTheta=False)
+    ent.set_model_options(ard_z=False, sl_w=False, sl_z=False, ard_w=False, factors=5, likelihoods=lik, learnTheta=False)
     ent.set_train_options(iter=10, tolerance=0.01, dropR2=0.0, seed=1, elbofreq=1)
 
     ent.build()
