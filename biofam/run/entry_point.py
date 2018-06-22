bf494b2416b96fa9a12f32db206abd901280b14d
damien.arnol <damien.arnol@gmail.com>
2018-06-22 18:06:28 +0100

    minor

--- biofam/run/entry_point.py
+++ biofam/run/entry_point.py
@@ -537,10 +537,10 @@ class entry_sfa(entry_point):
 
 if __name__ == '__main__':
     ent = entry_point()
-    dir = '/Users/damienarnol1/Documents/local/pro/PhD/FA/biofam/paper_figures/simul_data/stochastic_simul/tcga/'
-    infiles = [dir+'rna_breast', dir+'rna_ovarian']
-    views =  ["view_0", "view_0"]
-    groups = ["group_0", "group_1"]
+    dir = '/Users/damienarnol1/Documents/local/pro/PhD/FA/biofam/paper_figures/simul_data/stochastic_simul/'
+    infiles = [dir+'data_0_0.txt']
+    views =  ["view_0"]
+    groups = ["group_0"]
 
     # infiles = [dir+'data_0.txt']
     # views =  ["view_0"]
@@ -558,14 +558,14 @@ if __name__ == '__main__':
     lik = ["gaussian"]
     # lik = ["gaussian", "gaussian"]
     #
-    outfile = dir+"test_stochastic.hdf5"
+    outfile = dir+"test_no_stochastic.hdf5"
     #
     stochastic=True
     ent.set_data_options(lik, center_features=False, center_features_per_group=True, scale_features=False, scale_views=True)
-    ent.set_data_from_files(infiles, views, groups, delimiter=",", header_cols=True, header_rows=True)
-    ent.set_model_options(ard_z=True, sl_w=False, sl_z=False, ard_w=True, factors=5, likelihoods=lik)
-    ent.set_train_options(iter=50, tolerance=0.01, dropR2=0.0, seed=1, elbofreq=1, verbose=True)
-    if stochastic: ent.set_stochasticity_options(tau=1., forgetting_rate=0.1, batch_size=.2)  # TODO fix problem batch size > .5
+    ent.set_data_from_files(infiles, views, groups, delimiter=" ", header_cols=False, header_rows=False)
+    ent.set_model_options(ard_z=True, sl_w=False, sl_z=False, ard_w=True, factors=10, likelihoods=lik)
+    ent.set_train_options(iter=100, tolerance=0.01, dropR2=0.0, seed=1, elbofreq=10, verbose=True)
+    if stochastic: ent.set_stochasticity_options(tau=.8, forgetting_rate=0.05, batch_size=.2)  # TODO fix problem batch size > .5
     ent.build()
     ent.run(no_theta=False)
     ent.save(outfile)
