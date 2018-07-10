47f7d7ff2cfd62a4009b0d8516e7a854c0fddeac
Danila Bredikhin <danila.bredikhin@embl.de>
2018-07-10 10:01:38 +0100

    Fix cluster_samples function

--- BioFAMtools/R/cluster_samples.R
+++ BioFAMtools/R/cluster_samples.R
@@ -3,8 +3,8 @@
 ## Functions to cluster samples based on latent factors ##
 ##########################################################
 
-#' @title clusterSamples: K-means clustering on samples based on latent factors
-#' @name clusterSamples
+#' @title cluster_samples: K-means clustering on samples based on latent factors
+#' @name cluster_samples
 #' @description BioFAM factors are continuous in nature but they can be used to predict discrete clusters of samples, 
 #' similar to the iCluster model (Shen, 2009). \cr
 #' The clustering can be performed in a single factor, which is equivalent to setting a manual threshold; 
@@ -20,30 +20,32 @@
 #' @return output from \code{\link{kmeans}} function
 #' @export
 #' 
-clusterSamples <- function(object, k, factors = "all", ...) {
+cluster_samples <- function(object, k, factors = "all", ...) {
   
   # Sanity checks
   if (!is(object, "BioFAModel")) stop("'object' has to be an instance of BioFAModel")
   
 
   # Define factors
-  if (paste0(factors,collapse="") == "all") { factors <- factorNames(object) } 
-    else if(is.numeric(factors)) {
-      if (object@ModelOptions$LearnIntercept) factors <- factorNames(object)[factors+1]
-      else factors <- factorNames(object)[factors]
-    }
-      else{ stopifnot(all(factors %in% factorNames(object))) }
+  if (paste0(factors, collapse="") == "all") { factors <- factors_names(object) } 
+  else if(is.numeric(factors)) {
+    if (object@model_options$learn_intercept) factors <- factors_names(object)[factors+1]
+    else factors <- factors_names(object)[factors]
+  }
+  else {
+    stopifnot(all(factors %in% factors_names(object)))
+  }
   
   # Collect relevant data
-  Z <- getFactors(object, factors=factors, include_intercept=F)
-  N <- getDimensions(object)[["N"]]
+  Z <- get_factors(object, factors=factors, include_intercept=F)
+  if (class(Z) == "list") Z <- do.call(rbind, Z)
+  N <- nrow(Z)
   
   # For now remove sample with missing values on factors
   # (TO-DO) incorporate a clustering function that is able to cope with missing values
-  haveAllZ <- apply(Z,1, function(x) all(!is.na(x)))
+  haveAllZ <- apply(Z, 1, function(x) all(!is.na(x)))
   if(!all(haveAllZ)) warning(paste("Removing", sum(!haveAllZ), "samples with missing values on at least one factor"))
   Z <- Z[haveAllZ,]
-
   # Perform k-means clustering
   kmeans.out <- kmeans(Z, centers=k,  ...)
 
