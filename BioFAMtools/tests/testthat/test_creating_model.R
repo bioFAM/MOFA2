d05fe22c987b05f0659d865da680f54b87746f3c
Danila Bredikhin <danila.bredikhin@embl.de>
2019-02-26 15:34:51 +0100

    Fix the test for sparse matrices

--- BioFAMtools/tests/testthat/test_creating_model.R
+++ BioFAMtools/tests/testthat/test_creating_model.R
@@ -9,6 +9,17 @@ test_that("a model can be created from a list of matrices", {
 	expect_error(create_biofam(m))
 })
 
+test_that("a model can be created from a list of sparse matrices", {
+	skip_if_not_installed("Matrix")
+	# Generate a sparse matrix
+	m <- matrix(rnorm(100 * 5), ncol = 5) %*% t(matrix(rnorm(5 * 50), ncol = 5))
+	m[sample(1:nrow(m), 100, replace = TRUE), sample(1:ncol(m), 100, replace = TRUE)] <- 0
+	library(Matrix)
+	m <- Matrix(m, sparse = TRUE)
+	# Test if a sparse matrix can be imported to the BioFAModel
+	expect_is(create_biofam(list("view1" = m)), "BioFAModel")
+})
+
 test_that("a model can be created from a Seurat object", {
 	skip_if_not_installed("Seurat")
 	library(Seurat)
