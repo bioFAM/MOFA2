2cd5dba44855ab1510f5c53d6c54a8942759b42d
Danila Bredikhin <danila.bredikhin@embl.de>
2019-02-26 11:03:20 +0100

    Fix the test to load the model

--- BioFAMtools/tests/testthat/test_loading.R
+++ BioFAMtools/tests/testthat/test_loading.R
@@ -2,7 +2,6 @@ context("Loading the model")
 library(BioFAMtools)
 
 test_that("a pre-trained model can be loaded from disk", {
-  if (file.exists("test_biofam.hdf5"))
-    expect_is(load_model("test_biofam.hdf5"), "BioFAModel")
+  expect_is(load_model("test_biofam.hdf5"), "BioFAModel")
 })
 
