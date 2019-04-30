81d5bd880578012230f6390db50681531d503e6b
Danila Bredikhin <danila.bredikhin@embl.de>
2019-04-30 02:06:33 +0200

    Add newline symbol in the console in the verbose mode

--- biofam/core/BayesNet.py
+++ biofam/core/BayesNet.py
@@ -278,7 +278,7 @@ class BayesNet(object):
                 # Z = self.nodes["Z"].getExpectation()
                 # bar = s.mean(s.absolute(Z)<1e-3)
                 # print("Fraction of zero samples: %.0f%%" % (100*bar))
-            # print("\n")
+                print("\n")
 
             iter_time[i] = time()-t
             
