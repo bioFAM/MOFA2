87bd971cbee3d33329672770bde0dba7a12c68fe
rargelaguet <ricard.argelaguet@gmail.com>
2019-07-18 11:22:17 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,22 +3,23 @@ from setuptools import find_packages
 
 def setup_package():
   install_requires = ['pandas', 'scipy', 'numpy', 'sklearn', 'argparse', 'h5py']
-  # console_scripts = [ 'biofam=biofam.build_model.init_model:entry_point'],
   metadata = dict(
-      name = 'biofam',
+      name = 'mofa2',
       version = '0.1',
-      description = 'Bio Factor Analysis Models',
-      #long_description=read('README.rst'),
+      description = 'Multi-Omics Factor Analysis v2, a statistical framework for the integration of multi-group and multi-omics data',
       url = 'http://github.com/PMBio/biofam',
-      author = 'Ricard Argelaguet, Damien Arnol',
+      author = 'Ricard Argelaguet',
       author_email = 'ricard.argelaguet@gmail.com',
-      license = 'MIT',
+      license = 'LGPL-3.0',
       packages = find_packages(),
-      install_requires = install_requires,
-      #entry_points = {'console_scripts': console_scripts}
+      install_requires = install_requires
     )
 
   setup(**metadata)
 
+
 if __name__ == '__main__':
+  if sys.version_info < (2,7):
+    sys.exit('Sorry, Python < 2.7 is not supported')
+    
   setup_package()
