f5a0e0388b22b235da60e48d971737aa4bd91840
Danila Bredikhin <danila.bredikhin@embl.de>
2019-11-04 14:47:27 +0100

    Improve test_plots.R
    - Load model once, test all the plots having it loaded.
    - Use expect_silent for plots.
    - Plot heatmap and factor correlation silently.

--- MOFA2/tests/testthat/test_plots.R
+++ MOFA2/tests/testthat/test_plots.R
@@ -1,41 +1,42 @@
 context("Making plots")
 library(MOFA2)
 
+test_mofa2 <- load_model("test_mofa2.hdf5")
+
 # Data plots
 
 test_that("plot data overview works", {
-	expect_error(plot_data_overview(load_model("test_mofa2.hdf5")), NA)
+	expect_silent(p <- plot_data_overview(test_mofa2))
 })
 
 test_that("plot data heatmap", {
-	expect_error(plot_data_heatmap(load_model("test_mofa2.hdf5"), view = 1, factor = 1), NA)
+	expect_silent(p <- plot_data_heatmap(test_mofa2, view = 1, factor = 1, silent = TRUE))
 })
 
 test_that("plot data scatter", {
-	expect_error(plot_data_scatter(load_model("test_mofa2.hdf5"), view = 1, factor = 1), NA)
+	expect_silent(p <- plot_data_scatter(test_mofa2, view = 1, factor = 1))
 })
 
 test_that("plot data ASCII in terminal", {
-	expect_error(plot_ascii_data(load_model("test_mofa2.hdf5")), NA)
+	expect_error(plot_ascii_data(test_mofa2), NA)
 })
 
 
 # Plotting weights
 
 test_that("plot weights heatmap", {
-	expect_error(plot_weights_heatmap(load_model("test_mofa2.hdf5"), view = 1), NA)
+	expect_silent(p <- plot_weights_heatmap(test_mofa2, view = 1, silent = TRUE))
 })
 
 test_that("plot weights", {
-	expect_error(plot_weights(load_model("test_mofa2.hdf5"), view = 1, factors = 1:2), NA)
-})
-
-test_that("plot weights for one factor", {
-	expect_error(plot_weights(load_model("test_mofa2.hdf5"), factor = 1), NA)
+	# FOr multiple factors
+	expect_silent(p <- plot_weights(test_mofa2, view = 1, factors = 1:2))
+	# FOr one factor
+	expect_silent(p <- plot_weights(test_mofa2, factor = 1))
 })
 
 test_that("plot top weights", {
-	expect_error(plot_top_weights(load_model("test_mofa2.hdf5"), view = 1, factor = 1), NA)
+	expect_silent(p <- plot_top_weights(test_mofa2, view = 1, factor = 1))
 })
 
 
@@ -43,13 +44,13 @@ test_that("plot top weights", {
 # Plotting factor values
 
 test_that("plot factor values", {
-	expect_error(plot_factor(load_model("test_mofa2.hdf5")), NA)
+	expect_silent(p <- plot_factor(test_mofa2))
 })
 
 test_that("plot factor values", {
-	expect_error(plot_factors(load_model("test_mofa2.hdf5"), factors = 1:2), NA)
+	expect_silent(p <- plot_factors(test_mofa2, factors = 1:2))
 })
 
 test_that("plot factors correlation", {
-	expect_error(plot_factor_cor(load_model("test_mofa2.hdf5")), NA)
+	expect_silent({plot_factor_cor(test_mofa2); dev.off()})
 })
