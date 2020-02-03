37f856677c0907d18d09a8430e89500a5d124361
rargelaguet <ricard.argelaguet@gmail.com>
2020-02-03 14:03:46 +0000

    add version.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
+exec(open('mofapy2/version.py').read())
+
 def setup_package():
   install_requires = ['pandas', 'scipy', 'numpy', 'sklearn', 'argparse', 'h5py']
   metadata = dict(
       name = 'mofapy2',
-      version = '0.5.1',
+      version = __version__,
       description = 'Multi-Omics Factor Analysis v2, a statistical framework for the integration of multi-group and multi-omics data',
       url = 'http://github.com/bioFAM/MOFA2',
       author = 'Ricard Argelaguet',
@@ -24,3 +26,5 @@ if __name__ == '__main__':
     sys.exit('Sorry, Python < 2.7 is not supported')
     
   setup_package()
+
+
