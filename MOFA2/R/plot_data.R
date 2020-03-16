204a2823836a88981267c25a5adf22bc8f99bd0d
bv2 <britta.velten@gmail.com>
2020-03-16 15:35:15 +0100

    minor fix in plot_data

--- MOFA2/R/plot_data.R
+++ MOFA2/R/plot_data.R
@@ -103,7 +103,7 @@ plot_data_heatmap <- function(object, factor, view = 1, groups = "all", features
     
     # Predefined data.frame
     if (is.data.frame(annotation_samples)) {
-      annotation_samples <- annotation_samples[colnames(data),]
+      annotation_samples <- annotation_samples[colnames(data), , drop = FALSE]
       
     # Extract metadata from the sample metadata  
     } else if (is.character(annotation_samples)) {
