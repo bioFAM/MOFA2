7eab0d5eb07f7c5c83a24d63a5840e2d98ff8dc5
bv2 <britta.velten@gmail.com>
2020-08-28 10:50:19 +0200

    improved doc + names

--- mofapy2/simulate/simulate_mofa.py
+++ mofapy2/simulate/simulate_mofa.py
@@ -9,20 +9,23 @@ import matplotlib.pyplot as plt
 
 def simulate_data(N=200, seed=1234567, views = ["0", "1", "2", "3"], D = [500, 200, 500, 200], noise_level = 1,
                   K = 4, G = 1, lscales = [0.2, 0.8, 0.0, 0.0], sample_cov = "equidistant", scales = [1, 0.8, 0, 0],
-                  group_modelling = False, plot = False):
+                  model_groups = False, plot = False):
     """
     Function to simulate test data for MOFA (without ARD or spike-and-slab on factors)
-    noise_level: variance of the residuals (1/tau); for each feature this is multiplied by a uniform random number between 0.5 and 1.5 to model differences in feature's noise
-    scales, lscales: hyperparameters of the GP per factor
-    G: Number of groups
+
     N: Number of time points/ samples per group
+    seed: seed to use for simulation
+    views: list of view names
+    K: number of factors
+    G: Number of groups
+    D: list of number of features per view (same length as views)
+    noise_level: variance of the residuals (1/tau);
+                 per feature it is multiplied by a uniform random number in [0.5, 1.5] to model differences in features' noise
+    scales, lscales: hyperparameters of the GP per factor (length as given by K)
+    sample_cov: sample_covariates to use (can be of shape N X C) or "equidistant" or None
+    model_groups: If False group covariance is assumed to be 1 across all groups, else it is filled with 0,1s at random
+    plot: If True, simulation results are plotted
     """
-    # # set scales to 0 for lengthscale of 0 to obtain identity
-    # for k in range(K):
-    #     if lscales[k] == 0:
-    #         if scales[k] != 0:
-    #             scales[k] = 0
-    #             print("Setting scale of factor ", k, " to 0 as it has a lengthscale of 0 ")
 
     # simulate some test data
     np.random.seed(seed)
@@ -39,7 +42,7 @@ def simulate_data(N=200, seed=1234567, views = ["0", "1", "2", "3"], D = [500, 2
                 sample_cov = sample_cov.reshape(N, 1)
 
         # expand across groups
-        if not group_modelling:
+        if not model_groups:
             sample_cov = np.tile(sample_cov, (G,1))
         distC = SS.distance.pdist(sample_cov, 'euclidean')**2.
         distC = SS.distance.squareform(distC)
@@ -47,13 +50,7 @@ def simulate_data(N=200, seed=1234567, views = ["0", "1", "2", "3"], D = [500, 2
     else:
         lscales = [0]* K
 
-    if group_modelling:
-        rank_x = 2
-        sigma = 0
-        # x = np.random.binomial(1, 0.5, G * K * rank_x).reshape(K, rank_x ,G)
-        # Gmat = np.zeros([K, G, G])
-        # for k in range(K):
-        #     Gmat[k,:,:] = np.dot(x[k,:,:].transpose(),x[k,:,:]) + sigma * np.eye(G)
+    if model_groups:
         Gmat = np.random.binomial(1, 0.5, G * K * G).reshape(K, G ,G)
         for k in range(K):
             Gmat[k,:,:] = np.dot(Gmat[k,:,:], Gmat[k,:,:].transpose())
@@ -64,12 +61,12 @@ def simulate_data(N=200, seed=1234567, views = ["0", "1", "2", "3"], D = [500, 2
     for k in range(K):
         if lscales[k] > 0:
             Kmat = scales[k] * np.exp(-distC / (2 * lscales[k] ** 2))
-            if group_modelling:
+            if model_groups:
                 Kmat = np.kron(Gmat[k,:,:], Kmat)
             Sigma.append( Kmat + (1-scales[k]) * np.eye(N*G))
         elif lscales[k] == 0:
             Kmat = scales[k] * (distC == 0).astype(float)
-            if group_modelling:
+            if model_groups:
                 Kmat = np.kron(Kmat,  Gmat[k,:,:])
             Sigma.append(Kmat + (1-scales[k]) * np.eye(N*G))
             # Sigma.append(np.eye(N*G))
@@ -82,7 +79,7 @@ def simulate_data(N=200, seed=1234567, views = ["0", "1", "2", "3"], D = [500, 2
         for k in range(K):
             sns.heatmap(Sigma[k], ax =axs[k])
 
-    # simulate same factor values for factors with non-zero lengthscales across groups, other can differ (avoids expanding the covaraicen matrix across groups)
+    # simulate factor values
     Zks = []
     for k in range(K):
         sig = Sigma[k]
@@ -93,17 +90,18 @@ def simulate_data(N=200, seed=1234567, views = ["0", "1", "2", "3"], D = [500, 2
     for g in range(G):
         Z.append(Zks[groupidx == g,])
 
-    W = []
     # simulate alpha and theta, each factor should be active in at least one view
     inactive = 1000
     active = 1
     theta = 0.5 * np.ones([M, K])
-    alpha_tmp = [s.ones(M)*inactive]*K
+    alpha_tmp = [s.ones(M) * inactive]*K
     for k in range(K):
         while s.all(alpha_tmp[k]==inactive):
             alpha_tmp[k] = s.random.choice([active,inactive], size=M, replace=True)
     alpha = [ s.array(alpha_tmp)[:,m] for m in range(M) ]
 
+    # simulate weights
+    W = []
     for m in range(M):
         W.append(np.column_stack(
             [np.random.normal(0, np.sqrt(1/alpha[m][k]), D[m]) * np.random.binomial(1, theta[m][k], D[m]) for k in range(K)]))
@@ -114,6 +112,7 @@ def simulate_data(N=200, seed=1234567, views = ["0", "1", "2", "3"], D = [500, 2
         tau_m = stats.uniform.rvs(loc=0.5, scale=1, size=D[m]) * 1/noise_level # uniform between 0.5 and 1.5 scaled by noise level
         noise.append(np.random.multivariate_normal(np.zeros(D[m]), np.eye(D[m]) * 1 / np.lib.scimath.sqrt(tau_m), N))
 
+    # generate data
     data = []
     for m in range(M):
         tmp = []
@@ -123,22 +122,29 @@ def simulate_data(N=200, seed=1234567, views = ["0", "1", "2", "3"], D = [500, 2
 
     # store as list of groups
     if not sample_cov is None:
-        if not group_modelling:
+        if not model_groups:
             sample_cov = [sample_cov[groupidx == g] for g in range(G)]
         else:
             sample_cov = [sample_cov] * G
 
-    if not group_modelling:
+    if not model_groups:
         return {'data': data, 'W': W, 'Z': Z, 'noise': noise, 'sample_cov': sample_cov, 'Sigma': Sigma,
                 'views': views, 'lscales': lscales, 'N': N}
     else:
         return {'data': data, 'W': W, 'Z': Z, 'noise': noise, 'sample_cov': sample_cov, 'Sigma': Sigma,
                 'views': views, 'lscales': lscales, 'N': N, 'Gmat' : Gmat }
 
-# mask values at randomly sampled time points in each group and view
-# perc of time points are drawn in each view and group independtly and all feature values at this time point set to NaN
-# perc_all_views of time points are drawn in each group independently and all feature values in all views are set to NaN
+
+
 def mask_samples(sim, perc = 0.2, perc_all_views = 0):
+    """
+    Function to mask values at randomly sampled time points in each group and view.
+
+    Param:
+    perc: this fraction of time points are drawn in each view and group independently and all feature values at this time point set to NaN
+    perc_all_views: this fraction of time points are drawn in each group independently and all feature values in all views are set to NaN
+    """
+
     data = sim['data']
     N = sim['N']
     M = len(sim['views'])
