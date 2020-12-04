f0dc24ae2c1e3fe485291263c1657795cbe29adf
bv2 <britta.velten@gmail.com>
2020-12-04 08:34:23 +0100

    make MOFA2 R3.6 compatible by removal of basilisk '

--- R/run_mofa.R
+++ R/run_mofa.R
@@ -10,17 +10,13 @@
 #' The interface with Python is done with the \code{\link{reticulate}} package. 
 #' If you have several versions of Python installed and R is not detecting the correct one, 
 #' you can change it using \code{reticulate::use_python} when loading the R session. 
-#' Alternatively, you can let us install mofapy2 for you using \code{basilisk} if you set use_basilisk to \code{TRUE}
 #' @param object an untrained \code{\link{MOFA}} object
 #' @param save_data logical indicating whether to save the training data in the hdf5 file. 
 #'  This is useful for some downstream analysis (mainly functions with the prefix \code{plot_data}), but it can take a lot of disk space.
 #' @param outfile output file for the model (.hdf5 format). If \code{NULL}, a temporary file is created.
-#' @param use_basilisk use \code{basilisk} to automatically install a conda environment with mofapy2 and all dependencies? 
-#' If \code{FALSE} (default), you should specify the right python binary when loading R with \code{reticulate::use_python(..., force=TRUE)}
-#' or the right conda environment with \code{reticulate::use_condaenv(..., force=TRUE)}.
+#' @param use_basilisk Not available for R3.6
 #' @return a trained \code{\link{MOFA}} object
 #' @import reticulate
-#' @import basilisk
 #' @export
 #' @examples
 #' # Load data (in data.frame format)
@@ -34,7 +30,7 @@
 #' MOFAmodel <- prepare_mofa(MOFAmodel)
 #' 
 #' # Run the MOFA model
-#' \dontrun{ MOFAmodel <- run_mofa(MOFAmodel, use_basilisk = TRUE) }
+#' \dontrun{ MOFAmodel <- run_mofa(MOFAmodel) }
 run_mofa <- function(object, outfile = NULL, save_data = TRUE, use_basilisk = FALSE) {
   
   # Sanity checks
@@ -42,6 +38,10 @@ run_mofa <- function(object, outfile = NULL, save_data = TRUE, use_basilisk = FA
     stop("'object' has to be an instance of MOFA")
   if (object@status=="trained") 
     stop("The model is already trained! If you want to retrain, create a new untrained MOFA")
+  if(use_basilisk){
+    warning("basilisk not available for R3.6")
+    use_basilisk <- FALSE
+  }
   
   # If no outfile is provided, store a file in the /tmp folder with the respective timestamp
   if (is.null(outfile) || is.na(outfile) || (outfile == "")) {
@@ -58,32 +58,17 @@ run_mofa <- function(object, outfile = NULL, save_data = TRUE, use_basilisk = FA
   if (!use_basilisk) {
 
     message("Connecting to the mofapy2 python package using reticulate (use_basilisk = FALSE)... 
-    Please make sure to manually specify the right python binary when loading R with reticulate::use_python(..., force=TRUE) or the right conda environment with reticulate::use_condaenv(..., force=TRUE)
-    If you prefer to let us automatically install a conda environment with 'mofapy2' installed using the 'basilisk' package, please use the argument 'use_basilisk = TRUE'\n")
+    Please make sure to manually specify the right python binary when loading R with reticulate::use_python(..., force=TRUE) or the right conda environment with reticulate::use_condaenv(..., force=TRUE)\n")
     
     # Sanity checks
     have_mofa2 <- py_module_available("mofapy2")
     if (have_mofa2) {
       .run_mofa_reticulate(object, outfile, save_data)
     } else {
-      warning("mofapy2 is not detected in the specified python binary, see reticulate::py_config(). Setting use_basilisk = TRUE...")
-      use_basilisk <- TRUE
+      stop("mofapy2 is not detected in the specified python binary, see reticulate::py_config().")
     }
   }
     
-  # Connect to mofapy2 using basilisk (optional)
-  if (use_basilisk) {
-    
-    message("Connecting to the mofapy2 package using basilisk. 
-    Set 'use_basilik' to FALSE if you prefer to manually set the python binary using 'reticulate'.")
-    
-    proc <- basiliskStart(mofa_env)
-    on.exit(basiliskStop(proc))
-    tmp <- basiliskRun(proc, function(object, outfile, save_data) {
-      .run_mofa_reticulate(object, outfile, save_data)
-    }, object=object, outfile=outfile, save_data=save_data)
-  }
-  
   # Load the trained model
   object <- load_model(outfile)
   
